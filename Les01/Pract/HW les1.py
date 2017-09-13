3494336ea4cd32d67770e61487de8fe03fcfc38a
RWinning <erwin.van.tilburg@hotmail.com>
2017-09-13 13:09:29 +0200

    test van git

---
+++ Les01/Pract/HW les1.py
@@ -0,0 +1,10 @@
+lst = ['Berlioz', 'Borodin', 'Brian', 'Bartok', 'Bellini', 'Buxtehude', 'Bernstein']
+print(min(lst))
+print(max(lst))
+
+a = 6
+b = 7
+gemiddelde = (a + b) / 2
+print (gemiddelde)
+invetaris = ['papier', 'nietjes', 'pennen']
+print (invetaris)
